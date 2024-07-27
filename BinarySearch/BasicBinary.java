57d2826cb64ac974c950a3ba688d85045327e9c2
Ajaygattu781 <Ajay.Gattu@godigit.com>
2024-07-27 16:24:44 +0530

    Basic Binary Search

---
+++ BinarySearch/BasicBinary.java
@@ -0,0 +1,29 @@
+package BinarySearch;
+
+public class BasicBinary {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int key = 5;
+        int start = 0;
+        int end = arr.length - 1;
+        int res = BinarySearchArr(arr, start,end, key);
+        if(res!=-1)
+        System.out.println("Element found at index: " + res);
+        else
+            System.out.println("Element not found");
+    }
+
+    private static int BinarySearchArr(int[] arr, int start, int end, int key) {
+        while(start<=end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == key)
+                return mid;
+            else if (arr[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
